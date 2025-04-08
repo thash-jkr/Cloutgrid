eba09aadd479d17de920142ea70d4abbf96d66a3
thash-jkr <afthashsahal123@gmail.com>
2025-04-08 19:01:36 +0930

    Removed App directory

--- start_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script to run both React and Django servers in separate gnome-terminal windows
-gnome-terminal -- zsh -c "cd frontend && npm start; exec zsh"
-# Start Django server in a new terminal window
-gnome-terminal -- zsh -c "cd backend && source .djenv/bin/activate  && python manage.py runserver 0.0.0.0:8000; exec zsh"
-
